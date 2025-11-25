70e68d1003d2c16097b7c964da3db7406f352b38
lakshmi412t <183762073+lakshmi412t@users.noreply.github.com>
2025-11-25 18:03:32 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,  // REQUIRED for NavigationDrawer
+        useMaterial3: true,  
       ),
       home: HomePage(),
     );
